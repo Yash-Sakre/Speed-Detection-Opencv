e7a2682cef4322435795b351594fac01a87c534e
Yash Sakre <72202404+Yash-Sakre@users.noreply.github.com>
2021-12-28 21:10:43 +0530

    Update speed_det.py

--- speed_det.py
+++ speed_det.py
@@ -1,24 +1,33 @@
+#importing the necessary libraries
 import cv2
 import dlib
 import time
 import math
 
-
+#Taking Cascade file from the local directory 
 carCascade = cv2.CascadeClassifier('vech.xml')
+
+#initializing the video capture object 
 video = cv2.VideoCapture('carsVid.mp4')
 
-WIDTH = 1280
-HEIGHT = 720
+#To get the location of the object 
+WIDTH = 1280 #Width of the object
+HEIGHT = 720 #height of the object
 
+#To estimate the speed of the object using the previous and current location of the object 
+#The formula is taken from the paper "Real-Time Object Tracking using Adaptive Correlation Filters"
+#The formula is: speed = distance * fps * 3.6 
 def estimateSpeed(location1, location2):
     d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
-    # ppm = location2[2] / carWidht
     ppm = 8.8
     d_meters = d_pixels / ppm
     fps = 18
     speed = d_meters * fps * 3.6
     return speed
 
+#trackmultiobjects is the function which is used to detect the object in the video 
+#The function takes the video object as input and returns the location of the object
+#The function also returns the speed of the object 
 def trackMultipleObjects():
     rectangleColor = (0, 255, 0)
     frameCounter = 0
@@ -30,29 +39,29 @@ def trackMultipleObjects():
     carLocation1 = {}
     carLocation2 = {}
     speed = [None] * 1000
-
-    out = cv2.VideoWriter('outNew.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10, (WIDTH, HEIGHT))
-
-    while True:
+    
+    out = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc('M','P','4','V'), 10, (WIDTH, HEIGHT))
+    
+    while True: #Looping over the video using the video object
         start_time = time.time()
         rc, image = video.read()
         if type(image) == type(None):
             break
 
-        image = cv2.resize(image, (WIDTH, HEIGHT))
+        image = cv2.resize(image, (WIDTH, HEIGHT)) #resize the image to the desired size
         resultImage = image.copy()
 
         frameCounter = frameCounter + 1
         carIDtoDelete = []
 
-        for carID in carTracker.keys():
+        for carID in carTracker.keys(): #Looping over the carTracker dictionary
             trackingQuality = carTracker[carID].update(image)
 
             if trackingQuality < 7:
                 carIDtoDelete.append(carID)
 
         
-        for carID in carIDtoDelete:
+        for carID in carIDtoDelete: #Looping over the carIDtoDelete list
             print("Removing carID " + str(carID) + ' from list of trackers. ')
             print("Removing carID " + str(carID) + ' previous location. ')
             print("Removing carID " + str(carID) + ' current location. ')
@@ -101,7 +110,7 @@ def trackMultipleObjects():
 
                     currentCarID = currentCarID + 1
 
-        for carID in carTracker.keys():
+        for carID in carTracker.keys(): #Looping over the carTracker dictionary
             trackedPosition = carTracker[carID].get_position()
 
             t_x = int(trackedPosition.left())
@@ -118,7 +127,7 @@ def trackMultipleObjects():
         if not (end_time == start_time):
             fps = 1.0/(end_time - start_time)
 
-        for i in carLocation1.keys():
+        for i in carLocation1.keys(): #Looping over the carLocation1 dictionary
             if frameCounter % 1 == 0:
                 [x1, y1, w1, h1] = carLocation1[i]
                 [x2, y2, w2, h2] = carLocation2[i]
@@ -132,16 +141,17 @@ def trackMultipleObjects():
                     if speed[i] != None and y1 >= 180:
                         cv2.putText(resultImage, str(int(speed[i])) + "km/h", (int(x1 + w1/2), int(y1-5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 100) ,2)
 
-        cv2.imshow('result', resultImage)
+        cv2.imshow('result', resultImage) #Showing the result image
 
-        out.write(resultImage)
+        out.write(resultImage) #Writing the result image to the output video
 
-        if cv2.waitKey(1) == 27:
+        if cv2.waitKey(1) == 27: 
             break
 
     
-    cv2.destroyAllWindows()
-    out.release()
+    cv2.destroyAllWindows() #Destroying all the windows
+    out.release() #Releasing the output video
 
+#Calling the trackMultipleObjects function
 if __name__ == '__main__':
     trackMultipleObjects()
